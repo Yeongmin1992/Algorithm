7aec1a2a501795ce4e580a09c9e481484dfbb673
nym0101 <nym0101@echomarketing.co.kr>
2022-01-26 22:48:17 +0900

    orderic number

--- doit_algorithm_java/src/chap2/CardConvRev.java
+++ doit_algorithm_java/src/chap2/CardConvRev.java
@@ -1,22 +1,52 @@
 package chap2;
 
+import java.util.Scanner;
+
 // 변환한 기수의 자리수
 public class CardConvRev {
 
-    static int cardConvR(int x, int r) {
-        int digits = 0;
-        char[] d = new char[36];
+    static int cardConvR(int x, int r, char[] d) {
+        int digits = 0;   // 변환 후의 자릿수
         String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         do {
-            d[digits++] = dchar.charAt(x % r);
+            d[digits++] = dchar.charAt(x % r);    // r로 나눈 나머지를 저장
             x /= r;
         } while (x != 0);
-    return digits;
+
+        return digits;
     }
 
     public static void main(String[] args) {
-        System.out.println(CardConvRev.cardConvR(59, 16));
+        Scanner stdIn = new Scanner(System.in);
+        int no;     // 변환하는 정수
+        int cd;     // 기수
+        int dno;    // 변환 후의 자릿수
+        int retry;  // 다시 한 번?
+        char[] cno = new char[32];  // 변환 후 각 자리의 숫자를 넣어두는 문자의 배열
+
+        System.out.println("10진수를 기수 변환합니다.");
+        do {
+            do {
+                System.out.print("변환하는 음이 아닌 정수 : ");
+                no = stdIn.nextInt();
+            } while (no < 0);
+
+            do {
+                System.out.println("어떤 진수로 변환할까요? (2~36) : ");
+                cd = stdIn.nextInt();
+            } while (cd < 2 || cd > 36);
+
+            dno = cardConvR(no, cd, cno);
+
+            System.out.println(cd + "진수로는 ");
+            for (int i = dno -1; i >= 0; i--)
+                System.out.println(cno[i]);
+            System.out.println("입니다.");
+
+            System.out.println("한 번 더 할까요? (1.예/0.아니오) : ");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
     }
 }
 
