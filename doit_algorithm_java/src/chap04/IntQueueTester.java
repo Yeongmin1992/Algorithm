5f256c4eb974a5466190075d7841d478acbae588
nym0101 <nym0101@echomarketing.co.kr>
2022-03-23 08:24:36 +0900

    int queue test

--- doit_algorithm_java/src/chap04/IntQueueTester.java
+++ doit_algorithm_java/src/chap04/IntQueueTester.java
@@ -9,6 +9,25 @@ public class IntQueueTester {
 
         while (true) {
             System.out.println("현재 데이터 수 : " + s.size() + " / " + s.capacity());
+
+            System.out.print("(1)인큐  (2)디큐  (3)피크   " +
+                             "(4)덤프  (0)종료 : ");
+
+            int menu = stdIn.nextInt();
+            if (menu == 0) break;
+
+            int x;
+            switch (menu) {
+                case 1:
+                    System.out.print("데이터 : ");
+                    x = stdIn.nextInt();
+                    try {
+                        s.enque(x);
+                    } catch (IntQueue.OverflowIntQueueException e) {
+                        System.out.println("큐가 가득 찼습니다.");
+                    }
+                    break;
+            }
         }
     }
 }
