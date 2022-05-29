78726e43dad3aba9b0bc3fd2125cd07f9200f487
nym0101 <nym0101@echomarketing.co.kr>
2022-05-29 17:46:32 +0900

    int queue tester

--- doit_algorithm_java/src/chap04/IntQueueTester.java
+++ doit_algorithm_java/src/chap04/IntQueueTester.java
@@ -18,7 +18,7 @@ public class IntQueueTester {
 
             int x;
             switch (menu) {
-                case 1:
+                case 1:             // 인큐
                     System.out.print("데이터 : ");
                     x = stdIn.nextInt();
                     try {
@@ -26,6 +26,28 @@ public class IntQueueTester {
                     } catch (IntQueue.OverflowIntQueueException e) {
                         System.out.println("큐가 가득 찼습니다.");
                     }
+                    break;
+
+                case 2:             // 디큐
+                    try {
+                        x = s.deque();
+                        System.out.println("디큐한 데이터는 " + x + "입니다.");
+                    } catch (IntQueue.EmptyIntQueueException e) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+
+                case 3:             // 피크
+                    try {
+                        x = s.peek();
+                        System.out.println("피크한 데이터는 " + x + "입니다.");
+                    } catch (IntQueue.EmptyIntQueueException e) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
+
+                case 4:             // 덤프
+                    s.dump();
                     break;
             }
         }
